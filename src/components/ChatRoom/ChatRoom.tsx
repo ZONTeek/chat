fc9a89846b07653ea18a7a5eebf1da64eb6acaf6
GeorgyShustrov <gosha-s.1999@yandex.ru>
2022-04-04 01:42:13 +0300

    login styling

--- src/components/ChatRoom/ChatRoom.tsx
+++ src/components/ChatRoom/ChatRoom.tsx
@@ -1,4 +1,40 @@
 import styles from "./chatRoom.module.scss";
 export const ChatRoom = (): JSX.Element => {
-  return <div>chatroom</div>;
+  const currentUserId = 1;
+  const messages = [
+    { id: 1, text: "Привет", time: "06:00", senderId: 1 },
+    { id: 2, text: "Доброе утро", time: "06:01", senderId: 2 },
+    { id: 3, text: "Всем привет", time: "06:02", senderId: 3 },
+    { id: 4, text: "что по планам?", time: "06:02", senderId: 3 },
+    {
+      id: 5,
+      text: "Вы ебанулись, какие планы в 6 утра? Спать идите",
+      time: "06:03",
+      senderId: 4,
+    },
+  ];
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.room__header}></div>
+      <div className={styles.room}>
+        <div className={styles.room__chat}>
+          {messages.map((message) => {
+            <div className={styles.room__chat__message} key={message.id}>
+              <div className={styles.room__chat__message__sender}>
+                <img
+                  className={styles.room__chat__message__sender__ava}
+                  src=""
+                  alt=""
+                />
+                <div className={styles.room__chat__message__sender__name}>
+                  {message.senderId}
+                </div>
+              </div>
+            </div>;
+          })}
+        </div>
+        <div className={styles.room__form}></div>
+      </div>
+    </div>
+  );
 };
--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,10 +1,15 @@
 import { useFormik } from "formik";
+import styles from "./login.module.scss";
 import { useLocalStorage } from "hooks/useLocalStorage";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import eye from "../../assets/eye.png";
+import eye_disabled from "../../assets/eye_disabled.png";
 
 export const Login = (): JSX.Element => {
   const [username, setUsername] = useLocalStorage("username", "Guest");
   const navigate = useNavigate();
+  const [showPass, setShowPass] = useState(false);
   const formik = useFormik({
     initialValues: {
       username,
@@ -16,21 +21,45 @@ export const Login = (): JSX.Element => {
     },
   });
   return (
-    <form onSubmit={formik.handleSubmit}>
-      <input
-        name="username"
-        value={formik.values.username}
-        onChange={formik.handleChange}
-        placeholder="User name"
-      />
-      <input
-        name="password"
-        value={formik.values.password}
-        onChange={formik.handleChange}
-        placeholder="Password"
-        type={"password"}
-      />
-      <button type="submit">Submit</button>
-    </form>
+    <div className={styles.wrapper}>
+      <div className={styles.wrapper__title}>Login</div>
+      <form className={styles.form} onSubmit={formik.handleSubmit}>
+        <div className={styles.form__group}>
+          <label className={styles.form__group__label}>
+            User name
+            <input
+              className={styles.form__group__input}
+              name="username"
+              value={formik.values.username}
+              onChange={formik.handleChange}
+              placeholder="User name"
+            />
+          </label>
+        </div>
+        <div className={styles.form__group}>
+          <label className={styles.form__group__label}>
+            Password
+            <input
+              className={styles.form__group__input}
+              name="password"
+              value={formik.values.password}
+              onChange={formik.handleChange}
+              placeholder="Password"
+              type={showPass ? "text" : "password"}
+            />
+            <img
+              onMouseDown={() => setShowPass(true)}
+              onMouseUp={() => setShowPass(false)}
+              className={styles.form__group__toggle}
+              src={showPass ? eye : eye_disabled}
+              alt=""
+            />
+          </label>
+        </div>
+        <button className={styles.form__submit} type="submit">
+          Submit
+        </button>
+      </form>
+    </div>
   );
 };
